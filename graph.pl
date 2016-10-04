68ba9055ad7adae1cee2f609d0f646f98cd23007
Emanuele Salandin <esalandin@alice.it>
2016-10-04 18:09:19 +0200

    graph - added tests.

--- graph.pl
+++ graph.pl
@@ -9,3 +9,9 @@ rcon(A,B):- con(A,B).
 
 mcon(A,B,Cost,Path):- rcon(A,B), Cost is 1, Path = [B].
 mcon(A,B,Cost,Path):- rcon(A, _X), mcon(_X, B, _RemCost, _RemPath), Cost is _RemCost+1, Path=[_X|_RemPath].
+
+test:-
+	mcon(1,2,1,[2]),
+	mcon(1,5,3,[2,4,5]),
+	mcon(1,5,2,[3,5]),
+	true.
