a3b3a9c9541e12d8a612ea06a5cfdd528e78eadf
Emanuele Salandin <esalandin@alice.it>
2016-10-04 17:45:59 +0200

    graph - added cost calculation

--- graph.pl
+++ graph.pl
@@ -3,8 +3,9 @@ con(1,3).
 con(2,4).
 con(3,4).
 con(4,5).
+con(3,5).
 
 rcon(A,B):- con(A,B).
 
-mcon(A,B):- rcon(A,B).
-mcon(A,B):- rcon(A, _C), mcon(_C, B).
+mcon(A,B,Cost):- rcon(A,B), Cost is 1.
+mcon(A,B,Cost):- rcon(A, _X), mcon(_X, B, _RemCost), Cost is _RemCost+1.
