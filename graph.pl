c29f924afd68701907893d011cd8c758e3e7b9f0
Emanuele Salandin <esalandin@alice.it>
2016-10-05 11:56:04 +0200

    graph - indentation.

--- graph.pl
+++ graph.pl
@@ -8,7 +8,10 @@ con(3,5).
 rcon(A,B):- con(A,B).
 
 mcon(A,B,Cost,Path):- rcon(A,B), Cost is 1, Path = [B].
-mcon(A,B,Cost,Path):- rcon(A, _X), mcon(_X, B, _RemCost, _RemPath), Cost is _RemCost+1, Path=[_X|_RemPath], (memberchk(A, Path), !, fail; true).
+mcon(A,B,Cost,Path):-
+    rcon(A, _X), mcon(_X, B, _RemCost, _RemPath),
+    Cost is _RemCost+1, Path=[_X|_RemPath],
+	(memberchk(A, Path), !, fail; true).
 
 test:-
 	mcon(1,2,1,[2]),
