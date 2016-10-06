ccbfbbbb5c3ab565f2fd54a0a669e38e43f55799
Emanuele Salandin <esalandin@alice.it>
2016-10-06 11:48:21 +0200

    merge diverged

--- graph.pl
+++ graph.pl
@@ -5,14 +5,17 @@ con(3,4).
 con(4,5).
 con(3,5).
 
-rcon(A,B):- con(A,B).
-% rcon(A,B):- con(B,A).
+rcon(A,B):- con(A,B);con(B,A).
 
 mcon(A,B,Cost,Path):- rcon(A,B), Cost is 1, Path = [B].
 mcon(A,B,Cost,Path):-
-    rcon(A, _X), mcon(_X, B, _RemCost, _RemPath),
-    Cost is _RemCost+1, Path=[_X|_RemPath],
-	(memberchk(A, Path), !, fail; true).
+    rcon(A, _X),
+    mcon(_X, B, RemCost, RemPath),
+    newpath(_X, Path),
+    Cost is RemCost+1,
+    Path=[_X|RemPath].
+
+newpath(Node, Path) :- memberchk(Node, Path), !, fail; true.
 
 % path con il primo nodo in testa.
 mconp(A,B,C,P) :- mcon(A,B,C, _Path), P=[A|_Path].
@@ -30,11 +33,7 @@ list_min([L|Ls], Min0, Min) :-
 
 min_path_cost_couple(A,B,C):- path_cost_list(A,B,List), list_min(List, C).
 
-% trova percorso a costo minimo
 min_path(A,B,Path):- min_path_cost_couple(A,B,[_Cost, Path]).
-
-% come sopra, ma utilizzando finite domain (solo in gprolog),
-fd_min_path(A,B,Path):- fd_minimize(mconp(A,B,Cost,Path), Cost).
 	
 test:-
 	mcon(1,2,1,[2]),
@@ -42,5 +41,4 @@ test:-
 	mcon(1,5,2,[3,5]),
 	mconp(1,5,2,[1,3,5]),
 	min_path(1,5,[1,3,5]),
-	fd_min_path(1,5,[1,3,5]),
 	true.
